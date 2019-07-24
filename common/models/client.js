ffd8c284d5d609fcf923de821abb1cd748d488a9
Dindin Mahpudin <mahpudeen@gmail.com>
2019-07-24 14:56:45 +0700

    first commit

--- common/models/client.js
+++ common/models/client.js
@@ -4,5 +4,164 @@
 // License text available at https://opensource.org/licenses/MIT
 
 module.exports = function(Client) {
+    Client.remoteMethod(
+        'getNameDindin',
+        {
+            description: 'get name like -> Dindin',
+            accepts: [
+                { arg: 'firstname', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getNameDindin', verb: 'get' }
+        }
+    );
+
+    Client.getNameDindin = function(firstname, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    nama : {
+                        like : firstname
+                    }
+                }
+            }
+            Client.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Nama Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Client.remoteMethod(
+        'getAlamat',
+        {
+            description: 'get addresss like -> Cibinong',
+            accepts: [
+                { arg: 'alamat', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getAlamat', verb: 'get' }
+        }
+    );
+
+    Client.getAlamat = function(alamat2, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    alamat : {
+                        like : alamat2
+                    }
+                }
+            }
+            Client.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Nama Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Client.remoteMethod(
+        'getTelepon',
+        {
+            description: 'get addresss like -> Cibinong',
+            accepts: [
+                { arg: 'nomor telepon', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getTelepon', verb: 'get' }
+        }
+    );
+
+    Client.getTelepon = function(telepon2, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    telepon : {
+                        like : telepon2
+                    }
+                }
+            }
+            Client.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Nama Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Client.remoteMethod(
+        'getStatus',
+        {
+            description: 'get addresss like -> Cibinong',
+            accepts: [
+                { arg: 'alamat', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getStatus', verb: 'get' }
+        }
+    );
+
+    Client.getStatus = function(status2, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    status : {
+                        like : status2
+                    }
+                }
+            }
+            Client.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Nama Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
 
 };
--- common/models/report.js
+++ common/models/report.js
@@ -4,5 +4,164 @@
 // License text available at https://opensource.org/licenses/MIT
 
 module.exports = function(Report) {
+    Report.remoteMethod(
+        'getNameDindin',
+        {
+            description: 'get name like -> Dindin',
+            accepts: [
+                { arg: 'firstname', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getNameDindin', verb: 'get' }
+        }
+    );
+
+    Report.getNameDindin = function(firstname, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    nama_pelanggan : {
+                        like : firstname
+                    }
+                }
+            }
+            Report.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Nama Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Report.remoteMethod(
+        'getKodePesanan',
+        {
+            description: 'get kode like -> T123',
+            accepts: [
+                { arg: 'kodepesanan', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getKodePesanan', verb: 'get' }
+        }
+    );
+
+    Report.getKodePesanan = function(kodepesanan, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    kode_pesanan : {
+                        like : kodepesanan
+                    }
+                }
+            }
+            Report.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Kode Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Report.remoteMethod(
+        'getNamaBarang',
+        {
+            description: 'get nama barang like -> Antimo',
+            accepts: [
+                { arg: 'namabarang', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getNamaBarang', verb: 'get' }
+        }
+    );
+
+    Report.getNamaBarang = function(namabarang, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    nama_barang : {
+                        like : namabarang
+                    }
+                }
+            }
+            Report.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Kode Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
+
+    Report.remoteMethod(
+        'getHargaBarang',
+        {
+            description: 'get harga barang like -> Harga',
+            accepts: [
+                { arg: 'harga', type: 'string'}
+            ],
+            returns:{
+                arg: 'res', type:'object', root: true
+            },
+            http: { path: '/getHargaBarang', verb: 'get' }
+        }
+    );
+
+    Report.getHargaBarang = function(hargabarang, callback){
+        new Promise(function(resolve, reject){
+            var filter = {
+                where: {
+                    harga : {
+                        like : hargabarang
+                    }
+                }
+            }
+            Report.find(filter, function(err, result){
+                if(err) reject (err)
+                if(result === null){
+                    err = new Error ("Kode Tidak Dapat Ditemukan")
+                    err.statusCode = 404
+                    reject (err)
+                }
+                resolve(result)
+            })
+        }).then(function(res){
+            if (!res) callback (err)
+            return callback (null, res)
+        }).catch(function(err){
+            callback(err);
+        });
+    }
 
 };
